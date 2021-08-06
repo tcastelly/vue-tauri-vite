7012c773dd09df4d0094f1bf508f443534ceff36
Thomas CASTELLY <thomas@tcy.io>
2021-08-06 12:51:31 +0200

    replace workaround by real solution

--- init-wasm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# wasm build by running a `npm run dev` before the build
-# because vite will open a dev server, kill it by setting a wrong port
-
-npm run dev -- --port 1 --strictPort true &> /dev/null
-echo "init wasm done"
